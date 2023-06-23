359de036bc4df9b14690305f7c27f1f9413a3d70
Philip Paetz <philip.paetz@scalingfunds.com>
2023-06-23 13:39:18 +0200

    chore(bot): simplify isAcceptedMessageType()

--- apps/bot/src/middleware/isAcceptedMessageType.ts
+++ apps/bot/src/middleware/isAcceptedMessageType.ts
@@ -1,33 +1,30 @@
 import debug from "../utils/debug.js"
 import isCallback from "../utils/isCallback.js"
 import isTextMessage from "../utils/isTextMessage.js"
-import type { Context } from "telegraf"
-import type {
-  SceneContextScene,
-  WizardContextWizard,
-  WizardSessionData,
-} from "telegraf/scenes"
+import type { CustomContext } from "../types.js"
 
 // Checks if user input is a text or callback response.
 // Cancel if user replies with images, voice messages etc.
-export default async function isAcceptedMessageTypeMiddleware<
-  TContext extends Context & {
-    scene: SceneContextScene<TContext, WizardSessionData>
-    wizard: WizardContextWizard<TContext>
-  }
->(ctx: TContext, next: () => Promise<void>) {
+export default async function isAcceptedMessageTypeMiddleware(
+  ctx: CustomContext,
+  next: () => Promise<void>
+) {
   debug("[Middleware :: isTextMessage :: START")
   if (!isTextMessage(ctx.message) && !isCallback(ctx.update)) {
     debug(
       "User didn't reply with accepted message type, re-running same wizard step",
       ctx
     )
+    debug("[Middleware :: isTextMessage :: EXIT")
+
     await ctx.reply(
       "Sorry, I can't handle this message type. Please try again."
     )
-    ctx.wizard.selectStep(ctx.wizard.cursor) // Re-run the current step
-  } else {
-    debug("[Middleware :: isTextMessage :: END")
-    await next() // Continue to the next step or middleware
+
+    // Re-run the current step
+    return
   }
+
+  debug("[Middleware :: isTextMessage :: CONTINUE")
+  await next() // Continue to the next step or middleware
 }
